93189214f42d7eec3af68f84e9e90a87a7c504f7
Anilitka <119516870+Anilitka@users.noreply.github.com>
2024-01-25 18:43:30 +0400

    Update notification.service.ts

--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -15,7 +15,7 @@ openNotificationModal() {
   const token = this.tokenService.getToken();
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
     const timestamp = new Date().getTime();
-    const url = `https://mygpsadminbe.mygps.ge:4436/api/Manager/GetFilteredUsers?timestamp=${timestamp}`;
+    const url = `https://`;
 
     this.http.get(url, { headers, responseType: "json" }).subscribe({
       next: (data: any) => {

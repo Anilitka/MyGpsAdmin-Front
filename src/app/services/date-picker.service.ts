447794a37c35154e98aaa7c28740694bc86849f5
Anilitka <119516870+Anilitka@users.noreply.github.com>
2024-01-25 18:43:42 +0400

    Update date-picker.service.ts

--- src/app/services/date-picker.service.ts
+++ src/app/services/date-picker.service.ts
@@ -9,7 +9,7 @@ export class DatePickerService {
   constructor(private http: HttpClient) { }
 
   getCarListData(startDate: Date, endDate: Date, pageindex: number, pageSize: number) {
-    return this.http.get(`https://mygpsadminbe.mygps.ge:4436/api/Wialon/GetWialonFuelHistoryByDate?StartDate=${startDate}&EndDate=${endDate}&Pageindex=${pageindex}&PageSize=${pageSize}`);
+    return this.http.get(`https://`);
   }
 
 
